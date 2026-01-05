23475d6c2df4e2a27b156866adc4f2dee39dbb4f
angulardev2025 <angulardev2025@gmail.com>
2026-01-05 10:29:51 +0000

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone: true
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  time: string;
+  private intervalId: any;
 
+  constructor() {
+    this.time = new Date().toLocaleTimeString();
+  }
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.time = new Date().toLocaleTimeString();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
 }
